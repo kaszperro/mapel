9df00877c1c10fe327c30134a66cc9f1a01e7c02
ksapala <ksapala@qualtrics.com>
2022-06-05 17:41:16 +0200

    fix kk

--- projects/examples/example_kamada_kawai.py
+++ projects/examples/example_kamada_kawai.py
@@ -1,6 +1,6 @@
+import csv
+
 import mapel
-from mapel.main.features.distortion import calculate_distortion
-from mapel.main.features.monotonicity import calculate_monotonicity
 from mapel.main.features.stability import calculate_stability
 
 
@@ -16,10 +16,23 @@ def import_experiment():
                                               'emd-positionwise-paths-big-fixed-4_circle_kamada_bb_2_steps.csv',
                                               'emd-positionwise-paths-big-fixed-4_square_kamada_bb_2_steps.csv'
                                           ])
+    init_pos_path = 'experiments/emd-positionwise/coordinates/emd-positionwise-paths-big-ID-UN-AN-ST-bb.csv'
+    init_pos = _read_initial_positions(init_pos_path)
 
-    experiment.embed(algorithm='kamada-kawai')
+    experiment.embed(algorithm='kamada-kawai', init_pos=init_pos)
     experiment.print_map()
     print(calculate_stability(experiment))
+
+
+def _read_initial_positions(path):
+    output_dict = {}
+    with open(path, 'r', newline='') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=';')
+
+        for row in reader:
+            output_dict[row['instance_id']] = (row['x'], row['y'])
+
+    return output_dict
 
 
 if __name__ == '__main__':
